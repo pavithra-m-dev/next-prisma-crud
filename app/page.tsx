360ce6a2c66bc97fbb8929b4854a133189a472f9
Pavithra M <pavithram1642002@gmail.com>
2025-12-18 12:42:19 +0530

    Rectify the build error

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     fetchPosts();
   }, [fetchPosts]);
 
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false
-  }
+ // keep empty or add other configs later
 };
 
 export default nextConfig;
